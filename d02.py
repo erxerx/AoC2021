3de9b0f556ea44473b81fc361d443f574daaf634
Ervin Hülp <ervin.hylp@gmail.com>
2021-12-02 08:46:10 +0200

    Day 2

--- d02.py
+++ d02.py
@@ -3,7 +3,11 @@ with open('d02.in', 'r') as f:
     dd = [x.replace('\n', '').split(' ') for x in d]
     a = x = y = 0
     for i in dd:
-        if i[0] == 'forward': x += int(i[1]); y += a * int(i[1])
-        if i[0] == 'up': a -= int(i[1])
-        if i[0] == 'down': a += int(i[1])
+        if i[0] == 'forward':
+            x += int(i[1])
+            y += a * int(i[1])
+        if i[0] == 'up':
+            a -= int(i[1])
+        if i[0] == 'down':
+            a += int(i[1])
     print(x * y)  # part1 1938402, part2 1947878632
