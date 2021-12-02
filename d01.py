1b12c410afd36a47bcb940440e41ba3ae4cf4717
Ervin Hülp <ervin.hylp@gmail.com>
2021-12-02 12:25:19 +0200

    Indent fix

--- d01.py
+++ d01.py
@@ -1,12 +1,12 @@
 with open('d01.in', 'r') as f:
     d = f.readlines()
-    c = s = x = y = z = 0
-    for i in d:
-        z = y
-        y = x
-        x = int(i)
-        if x + y + z > s:
-            c += 1
-        s = x + y + z
-#    print(c - 1)  # part1 1195, subtract the 1st
-    print(c - 3)  # part2 1235, subtract the 1st 3
+c = s = x = y = z = 0
+for i in d:
+    z = y
+    y = x
+    x = int(i)
+    if x + y + z > s:
+        c += 1
+    s = x + y + z
+# print(c - 1)  # part1 1195, subtract the 1st
+print(c - 3)  # part2 1235, subtract the 1st 3
